95d556d551605b975818c7854fd8c5bdc14093a5
Andreas Papandreou <andreas27pap@gmail.com>
2019-04-22 14:14:26 +0300

    Add cmake-build-debug folder

---
+++ cmake-build-debug/CMakeFiles/Lab0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab0.dir/src/main.cpp.o"
+  "Lab0.pdb"
+  "Lab0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
